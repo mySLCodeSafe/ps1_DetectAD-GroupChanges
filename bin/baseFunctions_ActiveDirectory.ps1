3bd727ee19c5e9e82c1d3e2ec300990bae66d956
Shami Lakhani <email@shamilakhani.co.uk>
2019-01-23 14:57:34 +0000

    Inital

---
+++ bin/baseFunctions_ActiveDirectory.ps1
@@ -0,0 +1,52 @@
+﻿
+# Purpose: Query AD for a list of AD logons of the requested AD group 
+# Return: List of AD logons 
+function QueryADGroupForUsers 
+{
+    param
+    (
+        [Parameter (Mandatory=$true, HelpMessage="The name of the AD group you wish to query")]
+        [string]$ADGroupName
+    )
+
+    process
+    {
+        Try
+        {
+            $result=Get-ADGroupMember -Identity $ADGroupName | Select-Object -ExpandProperty "SamAccountName" | Sort-Object -Property SamAccountName 
+        }
+        Catch
+        {
+            throw $_
+            # $Result="Error"
+        }
+    }
+    end
+    {
+        return $result
+    }
+}
+
+
+# Purpose: Indicates if a requested AD account is enabed 
+# Returns True|False|Error
+function QueryADUserEnabledStatus
+{
+    param([string]$ADUser)
+
+    process
+    {
+        Try
+            {
+                $result=(Get-ADUser -Identity $ADUser | Select-Object -ExpandProperty "Enabled")
+            }
+        Catch
+            {
+                $result="Error"
+            }
+    }
+    end
+    {
+        return $result
+    }
+}
---
+++ go.ps1
@@ -0,0 +1,10 @@
+﻿# include:
+. .\bin\baseFunctions_ActiveDirectory.ps1
+
+
+# Script variables / Overide:
+$tmpFileName=get-date -Format FileDateTime
+$outputFolder=".\output"
+
+
+QueryADGroupForUsers "Accenture_Tech_Triage" | Out-File -FilePath $outputFolder\shami.csv
