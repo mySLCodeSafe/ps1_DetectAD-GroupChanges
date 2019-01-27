d4c4efe0934b5cfd3af987880ff1234c6bdbed32
Shami Lakhani <Shami.Lakhani@homeretailgroup.com>
2019-01-27 17:04:54 +0000

    Update go.ps1

--- go.ps1
+++ go.ps1
@@ -2,14 +2,16 @@
 . .\bin\core.ps1
 . .\bin\baseFunctions_ActiveDirectory.ps1
 
-# Script variables / Overide:
+# Script variables / Overide core:
 $ADGroupForQuery="Accenture_Tech_Triage"
+$lastRunDateFile=$inputFolder + "lastRun.data"
 
+# Declare functions:
 function validateChange([datetime]$lastRunDate, [datetime]$lastUpdateDate)
 {
     echo $lastRunDate
     echo $lastUpdateDate
-    if ($lastRunDate -ge $lastUpdateDate )
+    if ($lastUpdateDate -ge $lastRunDate )
     {
         return $true
     }
@@ -18,10 +20,27 @@ function validateChange([datetime]$lastRunDate, [datetime]$lastUpdateDate)
     }
 }
 
-
 # Main method:
-$ADQueryPropertyResult=(QueryADGroupProperties $ADGroupForQuery "whenChanged") #| Out-File $inputFolder\lastRun.data
 
-$lastRun=Get-Date '01/20/2019'
+try {
+    # when was the script last executed:
+    $input_lastRunDate = Get-Content -Path $lastRunDateFile | Convert-String
+    echo $input_lastRunDate
 
-echo (validateChange $lastRun $ADQueryPropertyResult)
+    $lastRunDate=[datetime]::ParseExact("201901241611", "yyyyMMddHHmm", $null)
+    #$lastRunDate=[datetime]::ParseExact($input_lastRunDate, "yyyyMMddHHmm", $null)
+    echo $lastRunDate
+
+
+    # when was the AD Group last modified:
+    $ADGrpLastUpdated=Get-ADGroup -Identity $ADGroupForQuery -Properties * | Select-Object -ExpandProperty "whenChanged"
+
+    # Did the AD group undergo a change after the last run
+    #echo (validateChange $lastRunDate $ADGrpLastUpdated)
+}
+catch {
+    throw $_
+}
+
+
+#Get-Date -Format yyyMMddHHmm
